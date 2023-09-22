643edc5c7f55836d98cd93a51d21cc8731411513
andersonlimaz <andersonlimaz@outlook.com>
2023-09-22 11:52:59 -0300

    commit db esta funcionando aparentemente

--- paginas/views.py
+++ paginas/views.py
@@ -9,22 +9,31 @@ from django.views.generic import TemplateView
 class PaginaInicial(TemplateView):
     template_name = "index.html"
 
+# views.py
+from django.shortcuts import render, redirect
+from django.views.generic import TemplateView
+from .forms import modelform
+
 class Doe(TemplateView):
-    template_name ="about-us.html"
+    template_name = "about-us.html"
+
+    def get(self, request, *args, **kwargs):
+        return self.render_to_response({'form': modelform(), 'enviado_com_sucesso': False})
 
     def post(self, request, *args, **kwargs):
         form = modelform(request.POST)
         if form.is_valid():
-            form.save()  
-            return redirect('página_de_sucesso')  
-        return self.render_to_response({'form': form})
+            form.save()
+            return redirect('página_de_sucesso')
+        return self.render_to_response({'form': form, 'enviado_com_sucesso': False})
+
 
 def form_modelform(request):
     if request.method == 'POST':
         form = modelform(request.POST)
         if form.is_valid():
-            form.save()  
-            return redirect('página_de_sucesso')  
+            form.save()
+            return redirect('página_de_sucesso')
     else:
         form = modelform()
 
